f294d3b9f28737afbcf1ec1935922b8ea56dbbf8
david <abovenormal5023@gmail.com>
2022-08-24 12:09:04 +0900

    applying gitignore

--- algo/src/programmers/Solution.java
+++ algo/src/programmers/Solution.java
@@ -1,4 +1,12 @@
 package programmers;
 
 public class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
