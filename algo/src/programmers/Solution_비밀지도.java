0810d9135ee51335a518295512d3886488fc9c11
한세환 <abovenormal5023@gmail.com>
2022-09-18 22:04:01 +0900

    프로그래머스 : 비밀지도 //카카오 블라인드 '18 stack을 이용하여 풀이 #refactor: 비트연산자를 활용하여 시간을 조금 더 단축시켜보았음.

--- algo/src/programmers/Solution_비밀지도.java
+++ algo/src/programmers/Solution_비밀지도.java
@@ -7,32 +7,24 @@ public class Solution_비밀지도 {
         String[] answer = {};
         answer = new String[n];
 
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        Stack<String> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
+
             StringBuilder sb = new StringBuilder();
-            int num1 = arr1[i];
-            int num2 = arr2[i];
+            int temp = arr1[i] | arr2[i];
 
-            for (int j = 0; j < n; j++) {       // 주어진 수 2진수로 변환
-                stack1.push(num1 % 2);
-                stack2.push(num2 % 2);
-                num1 = num1 / 2;
-                num2 = num2 / 2;
-            }
-
-            for (int j = 0; j < n; j++) {       // 앞의 자리 수부터 하나씩 비교
-                int temp1 = stack1.pop();
-                int temp2 = stack2.pop();
-
-                if (temp1 == 1 || temp2 == 1) {
-                    sb.append("#");
-                }
+            for(int j = 0; j<n;j++){
+                if(temp%2==1)
+                    stack.push("#");
                 else
-                    sb.append(" ");
+                    stack.push(" ");
+                temp /=2;
             }
 
+            for(int j=0;j<n;j++){
+                sb.append(stack.pop());
+            }
             answer[i] = sb.toString();
         }
 
