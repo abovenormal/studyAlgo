f27a4a650a98f052201d6160e1cde00d098b4f8c
Sehwan <shhan@emro.co.kr>
2024-03-11 23:06:55 +0900

    백준 24479 dfs

---
+++ algo/src/boj/Main_244779.java
@@ -0,0 +1,62 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_244779 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] check;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+
+        check = new int[n + 1];
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i < graph.size(); i++) {
+            Collections.sort(graph.get(i));
+        }
+        count = 1; // 시작점 포함.
+        dfs(graph, r);
+
+        for (int i = 1; i < check.length; i++) {
+            sb.append(check[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(ArrayList<ArrayList<Integer>> graph, int r) {
+        check[r] = count;
+
+        for (int i = 0; i < graph.get(r).size(); i++) {
+            int newStart = graph.get(r).get(i);
+
+            if (check[newStart] == 0) {
+                count++;
+                dfs(graph, newStart);
+            }
+        }
+    }
+}
